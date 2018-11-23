76e367a7f78dbf7e3d29124e8cddb31fe06ad436
Raul Metsma <raul@metsma.ee>
2018-11-23 15:28:38 +0200

    Update uninstall script, remove repository and keys (#35)
    Signed-off-by: Raul Metsma <raul@metsma.ee>

--- uninstall-open-eid.sh
+++ uninstall-open-eid.sh
@@ -7,3 +7,6 @@ sudo dpkg --purge \
   libdigidoc-common libdigidoc-tools libdigidoc2 \
   libdigidocpp-common libdigidocpp-tools libdigidocpp1 \
   qdigidoc4 qdigidoc qesteidutil qdigidoc-tera open-eid
+sudo rm /etc/apt/sources.list.d/ria-repository.list
+sudo apt-key del 592073D4
+sudo apt-key del C6C83D68
