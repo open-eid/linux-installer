80bcd84f73e423cefde56c3dfa14f44387662968
Kristel Merilain <kristel.merilain@ria.ee>
2023-06-19 09:56:58 +0300

    Add lunar support (#114)

--- install-open-eid.sh
+++ install-open-eid.sh
@@ -123,7 +123,8 @@ test_sudo
 # 20.04     focal   LTS   2025-04
 # 22.04     jammy   LTS   2027-04
 # 22.10     kinetic   -   2023-07
-LATEST_SUPPORTED_UBUNTU_CODENAME='kinetic'
+# 23.04     lunar   -   2024-01
+LATEST_SUPPORTED_UBUNTU_CODENAME='lunar'
 
 # check if Debian or Ubuntu
 distro=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
@@ -161,7 +162,7 @@ case $distro in
         utopic|vivid|wily|trusty|artful|cosmic|disco|xenial|eoan|groovy|hirsute|impish|bionic)
           make_fail "Ubuntu $codename is not officially supported"
           ;;
-        focal|jammy|kinetic)
+        focal|jammy|kinetic|lunar)
           add_repository $codename
           ;;
         *)
