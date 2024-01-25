7a4b6a097b32b37d959bb32ace652eb77e3e70a9
Arszilla <arszilla@arszilla.com>
2024-01-25 11:28:50 +0200

    Update uninstall-open-eid.sh
    - Use absolute paths instead of relative paths
    - Formatting and readability improvements

--- uninstall-open-eid.sh
+++ uninstall-open-eid.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-# This script removes open-eid .deb packages
+# This script removes DigiDoc4 Client (open-eid) packages
 
-sudo dpkg --purge \
-  opensc opensc-pkcs11 awp \
-  chrome-token-signing chrome-token-signing-policy firefox-pkcs11-loader \
-  token-signing-native token-signing-chrome token-signing-firefox \
-  web-eid web-eid-native web-eid-chrome web-eid-firefox \
-  libdigidoc-common libdigidoc-tools libdigidoc2 \
-  libdigidocpp-common libdigidocpp-tools libdigidocpp1 \
-  qdigidoc4 qdigidoc qesteidutil qdigidoc-tera open-eid
-sudo rm /etc/apt/sources.list.d/ria-repository.list
-sudo rm /etc/apt/trusted.gpg.d/ria-repository.gpg
-sudo rm /usr/share/keyrings/ria-repository.gpg
-sudo apt-key del 592073D4
-sudo apt-key del C6C83D68
+# Delete open-eid and opensc (and their dependencies):
+/usr/bin/sudo /usr/bin/dpkg --purge \
+    awp chrome-token-signing chrome-token-signing-policy \
+    firefox-pkcs11-loader libdigidoc-common libdigidoc-tools \
+    libdigidoc2 libdigidocpp-common libdigidocpp-tools libdigidocpp1 \
+    open-eid opensc opensc-pkcs11 qdigidoc qdigidoc-tera qdigidoc4 \
+    qesteidutil token-signing-chrome token-signing-firefox token-signing-native \
+    web-eid web-eid-chrome web-eid-firefox web-eid-native
+
+# Remove ria-repository.list from /etc/apt/sources.list.d/:
+/usr/bin/sudo rm /etc/apt/sources.list.d/ria-repository.list
+
+# Remove RIA's key from the trusted key set:
+/usr/bin/sudo rm /etc/apt/trusted.gpg.d/ria-repository.gpg
+/usr/bin/sudo rm /usr/share/keyrings/ria-repository.gpg
+/usr/bin/sudo /usr/bin/apt-key del C6C83D68
